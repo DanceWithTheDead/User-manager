bcde3d78c30e1c80a5d327239de9cf74aa71b3c5
DWTD <dwtdgm@gmail.com>
2026-02-13 14:40:26 +0300

    create user models

--- src/App.java
+++ src/App.java
@@ -1,4 +1,31 @@
+import java.util.Scanner;
+
+import models.User;
+
 public class App {
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        while (true) {    
+            
+            try {
+            System.out.print("Input Name (or 'exit' for exit): "); 
+            String name = input.nextLine();
+            if (name.equals("exit")) break;
+
+            System.out.print("Input Age: "); 
+            int age = input.nextInt();
+            input.nextLine();
+
+            System.out.print("Input Email: ");
+            String email = input.nextLine();
+            
+            User user = new User(name, age, email);
+            System.out.println(user);
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        input.close();
     }
 }
